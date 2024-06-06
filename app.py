3736ea324a5bbeeab43ad01bb01aa32c4855a853
kawon <kawon.jeong@gmail.com>
2024-06-06 13:05:04 +0900

    test

--- app.py
+++ app.py
@@ -3,4 +3,9 @@ import streamlit as st
 print("hello")
 
 st.title("Hello World")
-st.title("haha fun")
+
+with st.sidebar:
+    uploaded_files =  st.file_uploader("Upload your file",type=['pdf','docx'],accept_multiple_files=True)
+    process = st.button("Process")
+if process:
+    st.title("haha fun")
