12104735ca1a5386a5bbbda5e12543cb575fa196
kawon <kawon.jeong@gmail.com>
2024-06-09 20:27:54 +0900

    backup

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+if 'RUN_MODE' in globals() and RUN_MODE == "CLOUD":
+    __import__('pysqlite3')
+    import sys
+    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 import streamlit as st
 from langchain.prompts import (
